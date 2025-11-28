f8100fabbf7897c03789579bcf054c6882964892
Quan Zheng <qzheng@mac.lan>
2025-11-28 00:48:30 -0500

    fix cicd

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vitest.config.ts
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
   },
-})
+});
